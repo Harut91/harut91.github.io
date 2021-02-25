b439845011c585beedc650ea658bc4deae51ce19
Harut91 <78300135+Harut91@users.noreply.github.com>
2021-02-25 11:17:27 +0400

    js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+$('.owl-carousel').owlCarousel({
+  items:1,
+  lazyLoad:true,
+  loop:true,
+  margin:10,
+  dotsEach:true,
+});
